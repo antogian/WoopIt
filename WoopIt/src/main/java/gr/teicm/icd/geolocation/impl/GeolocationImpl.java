bad359f7f142d17881126b1164b3600691a12ddf
Panagiotis Roropoulos <p.roropoulos@gmail.com>
2015-11-24 18:37:59 +0200

    cleaned up

--- WoopIt/src/main/java/gr/teicm/icd/geolocation/impl/GeolocationImpl.java
+++ WoopIt/src/main/java/gr/teicm/icd/geolocation/impl/GeolocationImpl.java
@@ -10,20 +10,13 @@ public class GeolocationImpl implements Geolocation {
 		int R = 6371000; // Earth radius in meters
 		// Latitude degrees to radians
 		double f1 = lat1 * Math.PI / 180;
-		System.out.println(f1);
 		double f2 = lat2 * Math.PI / 180;
-		System.out.println(f2);
 		// Distances between latitudes and longitudes (radians)
 		double Df = (lat2-lat1) * Math.PI / 180;
-		System.out.println(Df);
 		double Dl = (lng2-lng1) * Math.PI / 180;
-		System.out.println(Dl);
 		double a = Math.sin(Df/2) * Math.sin(Df/2) + Math.cos(f1) * Math.cos(f2) * Math.sin(Dl/2) * Math.sin(Dl/2);
-		System.out.println(a);
 		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-		System.out.println(c);
 		double d = R * c;
-		System.out.println(d);
 		// if distance is less than radius1 + radius2 the circles are overlaping
 		if (d <= rad1+rad2)
 			return true;
