6280784a66566d44a6c35faddc29db5e7dc72481
Panagiotis Roropoulos <p.roropoulos@gmail.com>
2015-11-26 17:07:40 +0200

    More UserDAO tests

--- WoopIt/src/test/java/gr/teicm/icd/dao/UserDAOTest.java
+++ WoopIt/src/test/java/gr/teicm/icd/dao/UserDAOTest.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import gr.teicm.icd.data.entities.User;
 
@@ -55,5 +57,94 @@ public class UserDAOTest {
 			rs.close();
 			return false;
 		}
+	}
+	
+	public void insertPhotoPathTest(User user) throws SQLException {
+		Statement stmt = getConnection().createStatement();
+		String userPhotoPath = user.getUserPhotoPath();
+		String userName = user.getUserName();
+		stmt.executeUpdate("UPDATE USERS SET USER_PHOTO ='"+userPhotoPath+"' where USER_NAME='"+userName+"'");
+		
+	}
+	
+	public String getPhotoPathTest(String userName) throws SQLException {
+		Statement stmt = getConnection().createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT USER_PHOTO FROM USERS WHERE USER_NAME ='"+ userName +"'");
+		String userPhotoPath = null;
+		while (rs.next()) {
+			userPhotoPath = rs.getString("USER_PHOTO");
+		}
+		rs.close();
+		return userPhotoPath;
+	}
+	
+	public User getGeoLocationTest(String userName) throws SQLException {
+		Statement stmt = getConnection().createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT USER_LATITUDE, USER_LONGITUDE, USER_RADIUS FROM USERS WHERE USER_NAME ='"+ userName +"'");
+		
+		User user = new User();
+		if(rs.next()){
+			user.setUserLatitude(rs.getDouble("USER_LATITUDE"));
+			user.setUserLongitude(rs.getDouble("USER_LONGITUDE"));
+			user.setUserRadius(rs.getInt("USER_RADIUS"));
+		}
+		rs.close();
+		return user;
+	}
+	
+	public Boolean isFriendTest(User currentUser, User targetUser) throws SQLException {
+		Statement stmt = getConnection().createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM FRIENDS WHERE FRIENDS_CURRENT_USER_ID ='"+ currentUser.getUserId() +"' AND FRIENDS_TARGET_USER_ID='"+ targetUser.getUserId() +"'");
+		
+		if (rs.first()) {
+			rs.close();
+			return true;
+		}
+		else{
+			rs.close();
+			return false;
+		}
+	}
+	
+	public Boolean isBlockedTest(User currentUser, User targetUser) throws SQLException {
+		Statement stmt = getConnection().createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM BLOCKED WHERE BLOCKED_CURRENT_USER_ID ='"+ currentUser.getUserId() +"' AND BLOCKED_TARGET_USER_ID='"+ targetUser.getUserId() +"'");
+		
+		if (rs.first()) {
+			rs.close();
+			return true;
+		}
+		else{
+			rs.close();
+			return false;
+		}
+	}
+	
+	public List<User> getAllFriendsTest(User currentUser) throws SQLException {
+		List<User> allFriends = new ArrayList<>();
+		Statement stmt = getConnection().createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM FRIENDS WHERE FRIENDS_CURRENT_USER_ID ='"+ currentUser.getUserId() +"'");
+		
+		while(rs.next()){
+			User user = new User();
+			user.setUserId(rs.getLong("FRIENDS_TARGET_USER_ID"));
+			user = this.findByUserIdTest(user.getUserId());
+			allFriends.add(user);
+		}
+		return allFriends;
+	}
+	
+	public List<User> getAllBlockedTest(User currentUser) throws SQLException {
+		List<User> allBlocked = new ArrayList<>();
+		Statement stmt = getConnection().createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM BLOCKED WHERE BLOCKED_CURRENT_USER_ID ='"+ currentUser.getUserId() +"'");
+		
+		while(rs.next()){
+			User user = new User();
+			user.setUserId(rs.getLong("BLOCKED_TARGET_USER_ID"));
+			user = this.findByUserIdTest(user.getUserId());
+			allBlocked.add(user);
+		}
+		return allBlocked;
 	}
 }
--- WoopIt/src/test/java/gr/teicm/icd/dao/UserDAO_DBTest.java
+++ WoopIt/src/test/java/gr/teicm/icd/dao/UserDAO_DBTest.java
@@ -3,6 +3,9 @@ package gr.teicm.icd.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.dbunit.DatabaseTestCase;
 import org.dbunit.database.DatabaseConnection;
 import org.dbunit.database.IDatabaseConnection;
@@ -11,6 +14,7 @@ import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.junit.Test;
 
 import gr.teicm.icd.data.entities.User;
+import junit.framework.Assert;
 
 public class UserDAO_DBTest extends DatabaseTestCase {
 	private UserDAOTest userDaoTest;
@@ -50,5 +54,85 @@ public class UserDAO_DBTest extends DatabaseTestCase {
 		Boolean isExist;
 		isExist = userDaoTest.checkIfUserNameExistTest("TestUser");
 		assertTrue(isExist);
+	}
+	
+	@Test
+	public void testInsertPhotoPath() throws SQLException {
+		userDaoTest = new UserDAOTest();
+		User user = new User();
+		user.setUserName("TestUser");
+		user.setUserPhotoPath("default.png");
+		userDaoTest.insertPhotoPathTest(user);
+	}
+	
+	@Test
+	public void testGetUserPhotoPath() throws SQLException {
+		userDaoTest = new UserDAOTest();
+		String userPhotoPath = null;
+		userPhotoPath = userDaoTest.getPhotoPathTest("TestUser");
+		assertEquals(userPhotoPath, "default.png");
+	}
+	
+	@Test
+	public void testGetGeoLocation() throws SQLException {
+		userDaoTest = new UserDAOTest();
+		User user = new User();
+		
+		user = userDaoTest.getGeoLocationTest("TestUser");
+		assertEquals(user.getUserLatitude(), 22.2222222);
+		assertEquals(user.getUserLongitude(), 23.33333333);
+		assertEquals(user.getUserRadius(), 100);
+	}
+	
+	@Test
+	public void testIsFriend() throws SQLException {
+		userDaoTest = new UserDAOTest();
+		Boolean isFriend;
+		User currentUser = new User(); 
+		User targetUser = new User();
+		
+		currentUser.setUserId(1L);
+		targetUser.setUserId(2L);
+		
+		isFriend = userDaoTest.isFriendTest(currentUser, targetUser);
+		assertTrue(isFriend);
+	}
+	
+	@Test
+	public void testIsBlocked() throws SQLException {
+		userDaoTest = new UserDAOTest();
+		Boolean isBlocked;
+		User currentUser = new User(); 
+		User targetUser = new User();
+		
+		currentUser.setUserId(1L);
+		targetUser.setUserId(3L);
+		
+		isBlocked = userDaoTest.isBlockedTest(currentUser, targetUser);
+		assertTrue(isBlocked);
+	}
+	
+	@Test
+	public void testGetAllFriends() throws SQLException {
+		userDaoTest = new UserDAOTest();
+		User currentUser = new User();
+		currentUser.setUserId(1L);
+		List<User> allFriends = new ArrayList<>();
+		
+		allFriends = userDaoTest.getAllFriendsTest(currentUser);
+		
+		assertNotNull(allFriends);
+	}
+	
+	@Test
+	public void testGetAllBlocked() throws SQLException {
+		userDaoTest = new UserDAOTest();
+		User currentUser = new User();
+		currentUser.setUserId(1L);
+		List<User> allBlocked = new ArrayList<>();
+		
+		allBlocked = userDaoTest.getAllBlockedTest(currentUser);
+		
+		assertNotNull(allBlocked);
 	}
 }
