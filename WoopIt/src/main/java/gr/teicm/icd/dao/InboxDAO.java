cf750d2fcf59c6f7c4db2855862830bd6581266f
Panagiotis Roropoulos <p.roropoulos@gmail.com>
2015-11-29 21:01:07 +0200

    InboxDAO Interface COMPLETED

---
+++ WoopIt/src/main/java/gr/teicm/icd/dao/InboxDAO.java
@@ -0,0 +1,23 @@
+package gr.teicm.icd.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import gr.teicm.icd.data.entities.Inbox;
+import gr.teicm.icd.data.entities.User;
+@Repository
+public interface InboxDAO {
+	
+	 public void insertInbox(Inbox pm);
+	 
+	 public List<Inbox> getInbox(User user);
+	 
+	 public List<Inbox> getHistory(User user);
+	 
+	 public int getNewInboxQuantity(User user);
+	 
+	 public void deleteInbox(Long id, User user);
+	 
+	 public void resetNewInboxQuantity(User user);
+}
