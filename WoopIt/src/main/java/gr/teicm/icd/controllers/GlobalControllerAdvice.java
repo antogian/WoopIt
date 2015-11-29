3cd1786312eec2a39e3d1ddb7bcd06e9a01d29d3
Panagiotis Roropoulos <p.roropoulos@gmail.com>
2015-11-29 21:00:23 +0200

    GlobalControllerAdvice inbox notifications COMPLETED

--- WoopIt/src/main/java/gr/teicm/icd/controllers/GlobalControllerAdvice.java
+++ WoopIt/src/main/java/gr/teicm/icd/controllers/GlobalControllerAdvice.java
@@ -1,6 +1,9 @@
 package gr.teicm.icd.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
@@ -13,11 +16,24 @@ public class GlobalControllerAdvice {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private InboxService inboxService;
+	
 	@ModelAttribute("currentUser")
 	public User getCurrentUser(){
 		User currentUser = this.userService.getUserByName(this.userService.getLoggedInUsername());
 		//User currentUser2 = new User();
 		
 		return currentUser;
+	}
+	
+	@ModelAttribute("newNotifications")
+	public int getNewNotifications(){
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication(); 
+		if (!(auth instanceof AnonymousAuthenticationToken)) { 
+			return inboxService.getNewInboxQuantity(getCurrentUser());
+		}
+		else
+			return 0;
 	}
 }
