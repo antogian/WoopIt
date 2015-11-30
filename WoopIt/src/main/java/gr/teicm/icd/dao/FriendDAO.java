4ee7280a688c47f9341b296b3e661329a1f6cdc8
antogian <giant_27@yahoo.gr>
2015-11-30 18:35:35 +0200

    FriendDAO Interface COMPLETED

---
+++ WoopIt/src/main/java/gr/teicm/icd/dao/FriendDAO.java
@@ -0,0 +1,18 @@
+package gr.teicm.icd.dao;
+
+import java.util.List;
+
+import gr.teicm.icd.data.entities.User;
+
+public interface FriendDAO {
+	
+    public void addUserToFriends(User currentUser, User targetUser);
+    
+    public void removeUserFromFriends(User currentUser, User targetUser);
+    
+    public Boolean isFriend(User currentUser, User targetUser);
+    
+    public List<User> getAllFriends(User currentUser);
+    
+	
+}
