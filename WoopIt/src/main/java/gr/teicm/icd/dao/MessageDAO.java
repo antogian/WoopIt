217913a7270936efda36ea5e8cc6cbeb4212321d
theo357 <theodoulidistheodoros@gmail.com>
2015-12-02 19:54:25 +0200

    Update MessageDAO interface

--- WoopIt/src/main/java/gr/teicm/icd/dao/MessageDAO.java
+++ WoopIt/src/main/java/gr/teicm/icd/dao/MessageDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Repository;
 
 import gr.teicm.icd.data.entities.Message;
+import gr.teicm.icd.data.entities.User;
 @Repository
 public interface MessageDAO {
 	
@@ -12,4 +13,11 @@ public interface MessageDAO {
 	
 	public List<Message> getAllMessages();
 	
+	public void insertLike(Long messageId);
+	
+	public void insertDislike(Long messageId);
+	
+	public boolean checkIfLiked(User user, Long messageId);
+	
+	public void insertMessageUserLike(User user, Long messageId);
 }
