57e53efe48b44e2ac52c315cb421256649de4c70
theo357 <theodoulidistheodoros@gmail.com>
2015-12-10 16:57:52 +0200

    inbox history tested

---
+++ WoopIt/src/test/java/gr/teicm/icd/controllers/InboxHistoryTest.java
@@ -0,0 +1,83 @@
+package gr.teicm.icd.controllers;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.testSecurityContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.awt.List;
+
+import javax.servlet.Filter;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import static org.hamcrest.Matchers.*;
+import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
+
+@ContextConfiguration(locations = {
+		"file:src/main/webapp/WEB-INF/applicationContext.xml", 
+		"file:src/main/webapp/WEB-INF/spring/Spring-security.xml", 
+		"file:src/main/webapp/WEB-INF/spring/dispatcher-servlet.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@TestExecutionListeners(listeners={ServletTestExecutionListener.class,
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        WithSecurityContextTestExecutionListener.class})
+public class InboxHistoryTest {
+	
+	protected MockMvc mockMvc;
+	
+	@Autowired
+	private WebApplicationContext context;
+    
+	@Autowired
+	private Filter springSecurityFilterChain;
+	
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+	        		.webAppContextSetup(context)
+	        		.addFilters(springSecurityFilterChain)
+	                .build();
+    }
+	
+	@Test
+	@WithMockUser("test")
+	@Transactional
+	@Rollback(true)
+	public void testInboxHistoryView() throws Exception {
+        mockMvc.perform(get("/user/inboxHistory")
+	        .with(testSecurityContext()))
+	        .andExpect(status().isOk())
+	        .andExpect(model().size(3))
+	        .andExpect(model().attribute("allHistory", hasItem(allOf(hasProperty("body", is("hey"))))));
+	}
+
+	@Test
+	@WithMockUser("lalakis")
+	@Transactional
+	@Rollback(true)
+	public void testInboxEmptyHistoryView() throws Exception {
+        mockMvc.perform(get("/user/inboxHistory")
+	        .with(testSecurityContext()))
+	        .andExpect(status().isOk())
+	        .andExpect(model().size(3))
+	        .andExpect(model().attribute("allHistory", is(emptyCollectionOf(List.class))));
+	}
+}
