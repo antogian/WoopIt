e5d117c2609edd953c426cc657726aff26d4b5a6
theo357 <theodoulidistheodoros@gmail.com>
2015-12-10 16:58:39 +0200

    inbox View tested

---
+++ WoopIt/src/test/java/gr/teicm/icd/controllers/InboxViewTest.java
@@ -0,0 +1,111 @@
+package gr.teicm.icd.controllers;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.testSecurityContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.awt.List;
+
+import javax.servlet.Filter;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import static org.hamcrest.Matchers.*;
+import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
+
+@ContextConfiguration(locations = {
+		"file:src/main/webapp/WEB-INF/applicationContext.xml", 
+		"file:src/main/webapp/WEB-INF/spring/Spring-security.xml", 
+		"file:src/main/webapp/WEB-INF/spring/dispatcher-servlet.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@TestExecutionListeners(listeners={ServletTestExecutionListener.class,
+        DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        WithSecurityContextTestExecutionListener.class})
+public class InboxViewTest {
+	
+	protected MockMvc mockMvc;
+	
+	@Autowired
+	private WebApplicationContext context;
+    
+	@Autowired
+	private Filter springSecurityFilterChain;
+	
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders
+	        		.webAppContextSetup(context)
+	        		.addFilters(springSecurityFilterChain)
+	                .build();
+    }
+	
+	@Test
+	@WithMockUser("panos21")
+	@Transactional
+	@Rollback(true)
+	public void testInboxView() throws Exception {
+        mockMvc.perform(get("/user/inbox")
+	        .with(testSecurityContext()))
+	        .andExpect(status().isOk())
+	        .andExpect(model().size(3))
+	        .andExpect(model().attribute("allInbox", hasItem(allOf(hasProperty("body", is("hey")))))
+        );
+
+	}
+	
+	@Test
+	@WithMockUser("test")
+	@Transactional
+	@Rollback(true)
+	public void testInboxDelete() throws Exception {
+		mockMvc.perform(get("/user/inbox?delete=true&id=13")
+				.with(testSecurityContext()))
+				.andExpect(status().isOk())
+				.andExpect(model().size(3))
+				.andExpect(model().attribute("allInbox", is(emptyCollectionOf(List.class)))
+		);
+	}
+	
+	@Test
+	@WithMockUser("test")
+	@Transactional
+	@Rollback(true)
+	public void testInboxDeleteWithWrongParameterId() throws Exception {
+		mockMvc.perform(get("/user/inbox?delete=true&id=9999")
+				.with(testSecurityContext()))
+				.andExpect(status().isOk())
+				.andExpect(model().size(3))
+				.andExpect(model().attribute("allInbox", is(emptyCollectionOf(List.class)))
+		);
+	}
+	
+	@Test
+	@WithMockUser("test")
+	@Transactional
+	@Rollback(true)
+	public void testInboxDeleteWithWrongParameterDelete() throws Exception {
+		mockMvc.perform(get("/user/inbox?delete=xxx&id=13")
+				.with(testSecurityContext()))
+				.andExpect(status().is4xxClientError()
+		);        
+	}
+	
+}
